1d6b927655935f1911a5895edcb804f4d175af11
SnorlaxJS <craijonesslipknot@gmail.com>
2024-12-02 13:44:49 -0300

    Mudanças

---
+++ documentacao_estudo_js/mouse_eventos_estudo_js/scripts.js
@@ -0,0 +1,23 @@
+function mousedown() {
+    texto.innerHTML = `Pressionando o mouse`;
+}
+
+function mousemove() {
+    texto.innerHTML = `Mouse dentro`;
+}
+
+function mouseover() {
+    texto.innerHTML = `Mouse dentro`;
+}
+
+function mouseout() {
+    texto.innerHTML = `Mouse fora`;
+}
+
+function mouseup() {
+    texto.innerHTML = `botão clicado`;
+}
+
+function wheel() {
+    texto.innerHTML = `Girando a roda`;
+}
